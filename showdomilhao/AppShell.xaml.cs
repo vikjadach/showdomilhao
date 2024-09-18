b093e7b869a79cb2dbe988aae6d6f61544b6b56b
showdomilhao <victoriaferjc@gmail.com>
2024-09-18 10:37:27 -0300

    18/09/2024

---
+++ showdomilhao/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace showdomilhao;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ showdomilhao/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace showdomilhao;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
